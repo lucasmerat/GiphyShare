7cdb034fee5fa5df3129dbfa6777c4eef56a5d5d
Lucas Merat <lucas.merat@gmail.com>
2019-01-21 16:05:17 -0600

    Functionality to add buttons with search inputs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,15 +3,45 @@
 //Set up API query here
 //Key: VXGe08mkPRvKJZQIFGMHmx9xiIhGYg7t
 // URL: https://api.giphy.com/v1/gifs/search?api_key=&q=&limit=10&offset=0&rating=G&lang=en
-let query = "cheetah";
-let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=VXGe08mkPRvKJZQIFGMHmx9xiIhGYg7t&q=" + query + "&limit=10&offset=0&rating=G&lang=en";
+buttonArr = ['harry potter', 'hello', 'tiger', 'evil daemon babies'];
+loadButtons();
+function loadButtons(){
+    $("#button-area").empty();
+    for (let i=0;i<buttonArr.length;i++) {
+        let newButton = $('<button class="button">').text(buttonArr[i])
+        newButton.attr('data-number',i)
+        $('#button-area').append(newButton);
+    }
+    console.log(buttonArr)
+}
 
-$.ajax({url:queryUrl,method:'GET'}).then(giphyPull)
+function performCall() {
+  let query = $("#user-input").val();
+  let queryUrl =
+    "https://api.giphy.com/v1/gifs/search?api_key=VXGe08mkPRvKJZQIFGMHmx9xiIhGYg7t&q=" +
+    query +
+    "&limit=10&offset=0&rating=G&lang=en";
+    buttonArr.push(query);
+    loadButtons();
+  $.ajax({ url: queryUrl, method: "GET" }).then(giphyPull);
+}
 
-function giphyPull(response){
-    console.log(response);
+function performSearch(event) {
+  event.preventDefault($('#user-input').val());
+  console.log($('#user-input').val() === '')
+  if ($('#user-input').val() === ''){
+    alert('Enter a search to find giphys!');
+  } else{
+   performCall(); 
+ }
+}
+
+function giphyPull(response) {
+  console.log(response);
+  
 }
 
 //JS to add API data to page dynamically here
 
 //Running functions with event listeners here
+$("#search-btn").click(performSearch);
