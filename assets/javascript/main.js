e6599c444a752260727c703a10636770a0f34537
Lucas Merat <lucas.merat@gmail.com>
2019-01-22 11:53:08 -0600

    Added animated functionality to gifs, began addition of link overlay

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,4 @@
 //JS to alter page without API data here
-
-//Set up API query here
-//Key: VXGe08mkPRvKJZQIFGMHmx9xiIhGYg7t
-// URL: https://api.giphy.com/v1/gifs/search?api_key=&q=&limit=10&offset=0&rating=G&lang=en
 buttonArr = ["harry potter", "hello", "tiger", "evil daemon babies"];
 loadButtons();
 function loadButtons() {
@@ -13,6 +9,16 @@ function loadButtons() {
     $("#button-area").append(newButton);
   }
   console.log(buttonArr);
+}
+
+function performSearch(event) {
+  event.preventDefault();
+  console.log($("#user-input").val() === "");
+  if ($("#user-input").val() === "") {
+    alert("Enter a search to find giphys!");
+  } else {
+    performCall();
+  }
 }
 
 function performCall() {
@@ -26,38 +32,42 @@ function performCall() {
   $.ajax({ url: queryUrl, method: "GET" }).then(giphyPull);
 }
 
-function performSearch(event) {
-  event.preventDefault($("#user-input").val());
-  console.log($("#user-input").val() === "");
-  if ($("#user-input").val() === "") {
-    alert("Enter a search to find giphys!");
-  } else {
-    performCall();
-  }
-}
-
 function giphyPull(response) {
   console.log(response);
   console.log(response.data[0].images.fixed_width_still.url);
-  $('#image-area').empty();
-  $("#user-input").val('');
+  $("#image-area").empty();
+  $("#user-input").val("");
   for (let i = 0; i < response.data.length; i++) {
     console.log(response.data[i]);
     let image = $();
     $("#image-area").append(
       "<div class=image-box><img src =" +
         response.data[i].images.original_still.url +
-        " class=gif move=still>"
+        " class=gif animated=" +
+        response.data[i].images.original.url +
+        " still=" +
+        response.data[i].images.original_still.url +
+        "><div class=text><h4 class=facebook> <h4 class=twitter> <h4 class=download>"
     );
   }
 }
 
-function stillMove() {
-    //Add ability to change stills to animated
+function move() {
+  this.src = $(this).attr("animated");
+  $(this).siblings().children('.facebook').text('Share on Facebook')//selects the h4 and adds text
+  $(this).siblings().children('.twitter').text('Share on Twitter')//selects the h4 and adds text
+  $(this).siblings().children('.download').text('Copy download link')//selects the h4 and adds text
+
+}
+
+function still() {
+  this.src = $(this).attr("still");
+  $(this).siblings().children().empty()
 }
 
 //JS to add API data to page dynamically here
 
 //Running functions with event listeners here
 $("#search-btn").click(performSearch);
-$(".gif").click(stillMove); //Need to add functionality 
+$(document).on("mouseover", ".gif", move);
+$(document).on("mouseout", ".gif", still);
