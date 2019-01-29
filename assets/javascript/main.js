7340e85b5c6d2841549ee61a9dd4f759e5f65b26
Lucas Merat <lucas.merat@gmail.com>
2019-01-29 15:02:26 -0600

    Fixed up comments and formatting a bit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -37,7 +37,6 @@ function performSearch(event) {
 //Takes either button clicked innerText or value of user input bar and performs AJAX call
 function performCall(value) {
   let query = value;
-  console.log(query);
   buttonClicked = value; //Creating button clicked text variable gloabally to be used in giphy pull functions
   let queryUrl =
     "https://api.giphy.com/v1/gifs/search?api_key=VXGe08mkPRvKJZQIFGMHmx9xiIhGYg7t&q=" +
@@ -52,11 +51,11 @@ function performCall(value) {
     localStorage.setItem("savedButtons", JSON.stringify(buttonArr)); //Saves new list of buttons to local storage
   }
   loadButtons(); //Reloads buttons to display all values including new one added
-  console.log(firstCall);
-  //If the call to API was made by a button click, 
+  console.log("firstCall variable is" + firstCall);
+  //If the call to API was made by a button click
   if (event.target.className === "button") {
-    firstCall = true;//change first call value to true, making it reset values on next query
-    gifInitial = 10;//Restart search query at beggining of array
+    firstCall = true; //change first call value to true, making it reset values on next query
+    gifInitial = 10; //Restart search query at beggining of array
   }
   if (firstCall === true) {
     //If on first search, run initial giphy pull function
@@ -67,7 +66,7 @@ function performCall(value) {
     $.ajax({ url: queryUrl, method: "GET" }).then(giphyPullAdditional); //AJAX call takes giphyPull function as callback
   }
 }
-//Function to display gifs from API on page
+//Function to clear the page and show initial 10 gifs from API call
 function giphyPullInitial(response) {
   console.log(response); //Display JSON
   $("#image-area").empty(); //Clear all images, each query
@@ -79,6 +78,7 @@ function giphyPullInitial(response) {
       "'>Load 10 more</a>"
   );
   $("#user-input").val(""); //Clears user search bar
+  //Placing 10 images with social and gave buttons on page
   for (let i = 0; i < 10; i++) {
     $("#image-area").prepend(
       "<div class=image-box><img src =" +
@@ -94,14 +94,16 @@ function giphyPullInitial(response) {
         "'><a id=favorite class=social href='www.lucas.com'></a><i class='fas fa-heart fave-icon'>" //Link to favorite item
     );
   }
-  firstCall = false; //Setting boolean to run additional gif function if button is clicked
+  firstCall = false; //Changing boolean so additional image function will run next
   gifInitial += 10; //Increase initial gif value to show images farther in array on next call
   $(".footer").css("bottom", "auto"); //Sticking footer to bottom
 }
 //Pulls more gifs to bottom of page - How could I reuse the initial function above??
 function giphyPullAdditional(response) {
   console.log("Performing additional pull of images");
-  console.log(gifInitial);
+  console.log(
+    "Starting gif pull at the " + gifInitial + "th index of the data object"
+  );
   for (let i = gifInitial - 10; i < gifInitial; i++) {
     //Loop starts at 10, which increments every time you load gifs
     $("#image-area").append(
@@ -119,8 +121,8 @@ function giphyPullAdditional(response) {
         "'><a id=favorite class=social href='www.lucas.com'></a><i class='fas fa-heart fave-icon'>"
     );
   }
-  $(".load-more-box").appendTo("#image-area"); //Move the additional gif to end of page
-  gifInitial += 10; //Increase the gif initial value by 10
+  $(".load-more-box").appendTo("#image-area"); //Move the additional gif load button to end of page
+  gifInitial += 10;
 }
 
 //Changes gif to animated, called by hovering over image w mouseenter event
@@ -161,15 +163,15 @@ function still() {
     .children(".social")
     .css("visibility", "hidden");
 }
-
+//Pushes the favorite clicked to the faves array
 function saveFavorite(event) {
-  //Make it so that you can't save the same favorite twice
   event.preventDefault();
   favoritesArr.push(
     $(this)
       .siblings(".gif")
       .attr("src")
   );
+  //Puts faves array in local storage
   localStorage.setItem("savedImages", JSON.stringify(favoritesArr));
   //Heart beat animation on save of favorite
   $(this)
@@ -182,14 +184,13 @@ function removeFavorite() {
   let favoriteNumber = $(this).attr("data-fave");
   favoritesArr.splice(favoriteNumber, 1);
   displayFavorites();
-  localStorage.setItem("savedImages", JSON.stringify(favoritesArr));
+  localStorage.setItem("savedImages", JSON.stringify(favoritesArr)); //Resets local storage of array
 }
 //Displays favorite gifs on page - data pulled stored values in favorites array/Local Storage rather than API
 function displayFavorites() {
   event.preventDefault();
   $("#image-area").empty();
   for (let i = 0; i < favoritesArr.length; i++) {
-    console.log(favoritesArr[i]);
     $("#image-area").prepend(
       "<div class=image-box><img src=" +
         favoritesArr[i] +
